6b6c5ff639fa0088438b7fe8c8d32e0f24d089e3
MaelleVo <maellevolz@gmail.com>
2024-06-15 14:09:34 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
-dotenv.config(); // Charge les variables d'environnement depuis .env
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  dotenv.config({ path: `./.env.${mode}` }); // Charge les variables d'environnement depuis .env
+
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     define: {
       "process.env.REACT_APP_API_KEY": JSON.stringify(env.api_key),
