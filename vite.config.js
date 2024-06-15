2e2db60a9a98bb515c678269e84c35b7a7d17326
MaelleVo <maellevolz@gmail.com>
2024-06-15 14:16:28 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
@@ -6,7 +6,7 @@ dotenv.config(); // Load environment variables from .env file
 
 export default defineConfig(({ mode }) => {
   console.log(`Mode actuel: ${mode}`);
-  const env = process.env; // Directly use process.env
+  const env = loadEnv(mode, process.cwd(), ".env"); // Directly use process.env
 
   return {
     define: {
