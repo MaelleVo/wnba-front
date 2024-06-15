6da51aa7c3234b4e395c5a48fef6520d4e16576d
MaelleVo <maellevolz@gmail.com>
2024-06-15 15:06:11 +0200

    update

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,6 +2,10 @@ const Header = () => {
   return (
     <nav>
       <img className="logo-wnba" src="/WNBA-logo.png" alt="wnba-logo" />
+      <button>Teams</button>
+      <button>Seasons</button>
+      <button>Players</button>
+      <button>Schedule</button>
     </nav>
   );
 };
