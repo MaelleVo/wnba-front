4c0ca8e350c2b3209fcbe723008ad4c7c98d260d
MaelleVo <maellevolz@gmail.com>
2024-06-15 15:00:04 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
